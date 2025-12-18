d4225babd84c29f167833042d1427374f7479666
llzmgjzhy <963488649@qq.com>
2025-12-18 13:50:34 +0800

    linear model add pulseConv.

--- models/linear/linear_model.py
+++ models/linear/linear_model.py
@@ -1,5 +1,6 @@
 from typing import Optional
 import torch.nn as nn
+from models.one_fits_all.embed import DataEmbedding
 
 
 class Linear_Model(nn.Module):
@@ -10,13 +11,18 @@ class Linear_Model(nn.Module):
         self.pulse_len = config.pulse_len
         self.pulse_num = config.pulse_num
 
-        self.ln_proj = nn.LayerNorm(self.pulse_len * self.pulse_num)
-        self.out_layer = nn.Linear(self.pulse_len * self.pulse_num, self.num_classes)
+        self.enc_embedding = DataEmbedding(
+            config.pulse_len,
+            config.d_model,
+            dropout=config.dropout,
+        )
+        self.ln_proj = nn.LayerNorm(config.pulse_num * config.d_model)
+        self.out_layer = nn.Linear(config.pulse_num * config.d_model, self.num_classes)
 
     def forward(self, x_enc):
         B, L, M = x_enc.shape
-
-        outputs = x_enc.reshape(B, -1)
+        outputs = self.enc_embedding(x_enc, None)
+        outputs = outputs.reshape(B, -1)
         outputs = self.ln_proj(outputs)
         outputs = self.out_layer(outputs)
 
