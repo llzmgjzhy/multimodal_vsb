9982ee8baaae048da3f6237078eb3a3d8e306e33
llzmgjzhy <963488649@qq.com>
2026-02-10 17:52:05 +0800

    [VISION]: not frozen encoder.

--- models/swintransformer/swintransformer.py
+++ models/swintransformer/swintransformer.py
@@ -8,11 +8,7 @@ class SwinFeatureExtractor(nn.Module):
         self, model_name="microsoft/swin-tiny-patch4-window7-224", pretrained=True
     ):
         super().__init__()
-        self.backbone = (
-            SwinModel.from_pretrained(model_name)
-            if pretrained
-            else SwinModel.from_pretrained(model_name)
-        )
+        self.backbone = SwinModel.from_pretrained(model_name)
         self.hidden = self.backbone.config.hidden_size  # 768 for tiny
         # SwinModel 输出 last_hidden_state: [B, num_patches, hidden]
         # 我们做 mean pooling 得到 [B, hidden]
@@ -29,26 +25,27 @@ class DualImageSwinClassifier(nn.Module):
         self.model_name = config.model_pretrain
         # e.g., "microsoft/swin-tiny-patch4-window7-224"
         self.pretrained = True
-        self.freeze_backbone = True
+        self.freeze_backbone = False
         self.encoder = SwinFeatureExtractor(self.model_name, pretrained=self.pretrained)
 
         if self.freeze_backbone:
             for p in self.encoder.parameters():
                 p.requires_grad = False
-            self.encoder.eval()  # 注意：训练时你仍可以在 forward 外部切换 model.train()
 
         in_dim = self.encoder.hidden * 2  # heat + overlay concat
-        self.head = nn.Sequential(
-            nn.Linear(in_dim, 256),
-            nn.GELU(),
-            nn.LayerNorm(256),
-            nn.Dropout(0.2),
-            nn.Linear(256, 64),
-            nn.GELU(),
-            nn.LayerNorm(64),
-            nn.Dropout(0.2),
-            nn.Linear(64, 1),  # logit
-        )
+        # in_dim = self.encoder.hidden
+        # self.head = nn.Sequential(
+        #     nn.Linear(in_dim, 256),
+        #     nn.GELU(),
+        #     nn.LayerNorm(256),
+        #     nn.Dropout(0.2),
+        #     nn.Linear(256, 64),
+        #     nn.GELU(),
+        #     nn.LayerNorm(64),
+        #     nn.Dropout(0.2),
+        #     nn.Linear(64, 1),  # logit
+        # )
+        self.head = nn.Linear(in_dim, 1)  # 简单线性分类头
 
     def forward(self, x):
         # x 可以是 tuple/list: (x_heat, x_over)
